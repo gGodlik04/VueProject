1e203edfc20d7d8d91dc07fa3e4801a63c8e776f
Eduard Galikhanov <godlik04@mail.ru>
2023-09-25 18:41:22 -0700

    listener

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
-    <div>Hello world</div>
+    <div>
+        <div>
+            <button v-on:click="addLike">Like</button>
+            <button @click="addDislike">Dislike</button>
+        </div>
+        <div>Hello world</div>
+        <div>Count likes <strong>{{ likes }}</strong></div>
+    </div>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            likes: 6,
+        }
+    },
+    methods: {
+        addLike () {
+            this.likes += 1;
+        },
+        addDislike () {
+            this.likes -= 1;
+        }
+    }
+}
 </script>
 
 <style>
