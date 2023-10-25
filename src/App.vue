5f2009c5995743a22d16fee875faa137be9b56e0
Eduard Galikhanov <godlik04@mail.ru>
2023-10-25 14:00:34 -0700

    style: refactor style + router

--- src/App.vue
+++ src/App.vue
@@ -1,180 +1,18 @@
 <template>
-    <div class="container">
-        <h1 
-            class="title"
-        >
-            Страница с постами
-        </h1>
-        <InputComponent
-            class="app_search" 
-            v-model="searchQuery"
-            placeholder="Поиск..."
-        />            
-        <div class="app_buttons">
-            <Button 
-                class="post-form-add" 
-                @click="modalShow"
-            >
-                Добавить пост
-            </Button>
-            <SelectPart 
-                v-model="selectedSort"
-                :options="sortOptions"
-            />
-        </div>
-        <ModalWindow v-model:show="modalVisible">
-            <PostForm
-            @create="createPost"
-            />
-        </ModalWindow>
-        <PostList 
-            v-if="!isLoading"
-            :posts="searchInSortedPosts"
-            @removePost="removePost"
-        />
-        <ModalLoading v-model:show="isLoading"
-            class="loading" 
-            v-if="isLoading"
-        >
-            Идет загрузка...
-        </ModalLoading>
-        <div ref="observerBlock" class="observer-block"></div>
-        <!-- <div class="page-wrapper">
-            <div 
-                v-for="pageNumber in totalPage" 
-                class="page__item"
-                :class="{
-                    'page-active': page === pageNumber
-                }"
-                :key="pageNumber"
-                @click="changePage(pageNumber)"
-            >
-                {{ pageNumber }}
-            </div>
-        </div> -->
+    <div class="app">
+        <router-view></router-view>
     </div>
 </template>
-
 <script>
-import PostForm from "@/components/PostForm.vue";
-import PostList from "@/components/PostList.vue";
-import axios from 'axios';
-
 export default {
-    components: {
-    PostForm, PostList,
-},
-    data() {
-        return {
-            posts: [],
-            modalVisible: false,
-            isLoading: false,
-            selectedSort: '',
-            sortOptions: [
-                {value: 'title', name: 'По названию'},
-                {value: 'body', name: 'По описанию'},
-            ],
-            searchQuery: '',
-            page: 1,
-            limit: 10,
-            totalPage: 0,
-        }
-    },
-    methods: {
-       createPost(post) {
-            this.posts.push(post);
-            this.modalVisible = false;
-       },
-       removePost (post) {
-            this.posts = this.posts.filter(p => p.id !== post.id);
-       },
-       modalShow() {
-        this.modalVisible = true;
-       },
-    //    changePage(pageNumber) {
-    //         this.fetchPosts();
-    //         this.page = pageNumber;
-    //    },
-       async fetchPosts() {
-        try {
-                this.isLoading = true;
-                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-                    params: {
-                        _page: this.page,
-                        _limit: this.limit
-                    }
-                })
-                .then(res => {
-                    this.totalPage = Math.ceil(res.headers['x-total-count'] / this.limit)
-                    this.posts = res.data;    
-                });
-        } catch (error) {
-            console.log(error);
-        } finally {
-            this.isLoading = false;
-        }
-       },
-       async loadMorePosts() {
-        try {
-                this.page++;
-                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-                    params: {
-                        _page: this.page,
-                        _limit: this.limit
-                    }
-                })
-                .then(res => {
-                    this.totalPage = Math.ceil(res.headers['x-total-count'] / this.limit)
-                    this.posts = [...this.posts, ...res.data];
-                });
-        } catch (error) {
-            console.log(error);
-        }
-       },
-    },
-    mounted() {
-        this.fetchPosts();
-        const options = {
-            rootMargin: "0px",
-            threshold: 0.1,
-        };
-        const callback = (entries, observer) => {
-            if (entries[0].isIntersecting) {
-                this.loadMorePosts();
-            }
-        };
-        const observer = new IntersectionObserver(callback, options);
-        observer.observe(this.$refs.observerBlock);
-    },
-    computed: {
-        sortPosts() {
-            return [...this.posts].sort((post1, post2) => {
-                return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
-            })
-        },
-        searchInSortedPosts() {
-            return this.sortPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
-        }
-    },  
-    watch: {
-        // loadPage() {
-        //     this.fetchPosts();
-        // }
-    }
+    
 }
 </script>
-
 <style>
-    * {
+  * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-    }
-
-    body {
-        background-color: #002451;
-        color: #ffffff;
-        padding: 0 5em 0 5em;
     }
 
     .container {
@@ -182,54 +20,5 @@ export default {
         margin-left: auto;
         margin-right: auto;
         margin-top: 50px;
-    }
-    
-    .loading {
-        margin: 0;
-    }
-
-    .title {
-        text-align: center;
-    }
-
-    .post-form-add {
-        text-align: center;
-        margin-top: 25px;
-        width: 15%;
-    }
-
-    .app_buttons {
-        display: flex;
-        justify-content: space-between;
-    }
-
-    .app_search {
-        margin-top: 25px;
-    }
-
-    .page-wrapper {
-        display: flex;
-        margin: 50px 0 50px 0;
-        justify-content: center;
-    }
-    
-    .page__item {
-        margin-right: 10px;
-        font-size: 20px;
-        padding: 5px;
-        border: 4px #fff solid;
-        border-radius: 2px;
-        text-align: center;
-        min-width: 2em;
-    }
-
-    .page-active {
-        background-color: #fff;
-        border: 4px #000 solid;
-        color: #000;
-    }
-
-    .observer-block {
-        height: 10px;
     }
 </style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,44 @@
+<template>
+    <h1 class="title">Hell0, m' so 9lad to see y (*-*)</h1>
+    <div class="container">
+        <div class="nav-wrapper">
+            <h1 class="nav__items">User page with posts</h1>
+            <h1 class="nav__items">Page with something else</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+    .nav-wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    
+    .nav__items {
+        text-align: center;
+        min-width: 400px;
+        padding: 10px;
+        border: 4px solid #320087;
+        background-color: rgb(36, 87, 228);
+        color: #fff;
+        border-radius: 5px;
+        margin-top: 40px;
+        align-self: center;
+    }
+
+    .title {
+        margin-top: 25px;
+        text-align: center;
+        padding: 10px;
+        border: 4px solid rgb(36, 87, 228);
+        background-color: #320087;
+        color: #fff;
+        border-radius: 5px;
+    }
+</style>
